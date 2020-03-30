b5568bdc33dbb346cc17bbd8a1f0f656cd2ebe12
Julianto <juliwibowo@outlook.com>
2020-03-30 13:52:06 +0700

    Berhasil hapus data

--- controller.js
+++ controller.js
@@ -65,3 +65,16 @@ exports.ubahmahasiswa = function(req,res){
     });
     
 };
+
+//menghapus data berdasarkan id
+exports.hapusmahasiswa = function(req,res){
+    var id = req.body.id_mahasiswa;
+    connection.query('DELETE FROM mahasiswa WHERE id_mahasiswa=?',[id],
+    function(error, rows, fields){
+        if(error){
+            console.log(error);
+        }else {
+            response.ok("Berhasil Hapus data", res)
+        }
+    });
+};
