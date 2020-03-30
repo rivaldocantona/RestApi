fb91543f6bbdd907817da0cd66efc239ec5f2fec
Julianto <juliwibowo@outlook.com>
2020-03-30 13:14:37 +0700

    berhasil menambahkan data mahasiswa

--- controller.js
+++ controller.js
@@ -4,6 +4,15 @@ var response = require('./res');
 var connection = require('./koneksi');
 
 exports.index = function(req,res){
-    response.ok("Aplikasi RES API berjalan")
-
+    response.ok("Aplikasi RES API berjalan",res)
+};
+//MEnambahkan data mahasiswa
+exports.tampilsemuamahasiswa = function(req,res){
+    connection.query('SELECT * FROM mahasiswa', function(error, rows, fileds){
+        if(error){
+            console.log(error);
+        }else {
+            response.ok(rows, res)
+        }
+    });
 };
